87137e6a470efbd385895c74af70fead3a3682e2
Shanmugapriya Sethuraman <Shanmugapriya@rhodes.mu>
2025-10-13 15:07:40 +0400

    [Updated code after 13-10-2025]

--- afrex_supply_chain/models/asc_approval.py
+++ afrex_supply_chain/models/asc_approval.py
@@ -59,3 +59,17 @@ class Approval(models.Model):
             'approve_uid': False,
             'approve_date': False,
         })
+
+from odoo import models, api
+
+class AccountIncoterms(models.Model):
+    _inherit = 'account.incoterms'
+
+    @api.model
+    def _name_search(self, name='', args=None, operator='ilike', limit=100, name_get_uid=None, order=None):
+        args = args or []
+        domain = []
+        if name:
+            domain = ['|', ('code', operator, name), ('name', operator, name)]
+        return self._search(domain + args, limit=limit, access_rights_uid=name_get_uid, order=order)
+
